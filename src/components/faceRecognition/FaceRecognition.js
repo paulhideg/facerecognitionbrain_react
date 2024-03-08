9b6092d39072bda66e171ff558dbda17288ef550
paulhideg <paulhideg002@gmail.com>
2024-03-08 10:57:46 +0200

    Fixed image

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -5,13 +5,14 @@ const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="center">
       <div className="absolute mt2">
-        <img
+        {/* <img
           id="inputImage"
-          alt="sample"
+          alt="missing"
           src={imageUrl}
           width="500px"
           height="auto"
-        />
+          display="none"
+        /> */}
         <div
           className="bounding-box"
           style={{
